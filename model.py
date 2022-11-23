bb00d048c8f5fe59bac834ec90354e20018a605d
Vinicius Ferreira Raymundo <vinicius.raymundo@willbank.com.br>
2022-11-23 19:37:56 -0300

    Adicionar exercícios

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ class Produto:
     def calcularDesconto(self, porcentagem):
         resultado = (self.preco*porcentagem)/100
         return self.preco - resultado
+    
+    #faça um metodo para comparaar se o nome e a marca do produto são iguias a do objeto que ele está comparando. 
 
 class RegistroDeEstoque:
     def __init__(self, quantidade, produto: Produto) -> None:
@@ -17,6 +19,8 @@ class RegistroDeEstoque:
     def __repr__(self):
         return "\n"+str(self.produto.nome) + " \n" + "Quantidade: " + str(self.quantidade)+"\n"
     
+    #Faço um metodo de removerDoEstoque, passando o produto e subtraindo 1 da quantidade do registro
+
 class Loja:
     def __init__(self):
         self.estoque = []
@@ -26,3 +30,6 @@ class Loja:
 
     def exibirEstoque(self):
         return self.estoque
+
+    #Faça um metodo que possibilidade encontrar um produto da Atributo estoque
+    #Faça um metodo de vender que deve receber como parametro um produto e SUBTRAIR 1 do Registro de Estoque
